b9db98ac63adb87298e403c2da0006052a239017
Nafha-krs <nafhakrs@gmail.com>
2026-07-24 16:37:05 +0100

    Configure Swagger and add JUnit tests

--- backend/src/main/java/com/alten/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/alten/backend/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "/auth/register").permitAll()
+                        .requestMatchers("/auth/login", "/auth/register", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)

a0f1831bfbaae62682b466785d41c9fed04eda4a
Nafha-krs <nafhakrs@gmail.com>
2026-07-30 15:08:18 +0100

    fix: configuration vite et maj dependances

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import autoprefixer from 'autoprefixer'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig(() => {
   return {
